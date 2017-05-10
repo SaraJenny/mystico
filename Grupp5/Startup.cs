9da48f4f1e0636d676ede75f1341a9048c022395
Sara Pettersson <sara@doggie-zen.se>
2017-05-10 10:52:34 +0200

    Lagt till StaticFiles i Startup + lagt till css

--- Grupp5/Startup.cs
+++ Grupp5/Startup.cs
@@ -40,6 +40,7 @@ namespace Grupp5
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
+			app.UseStaticFiles();
 			app.UseDeveloperExceptionPage();
 			app.UseIdentity();
 			app.UseMvcWithDefaultRoute();
