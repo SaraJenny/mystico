c3902a89df87ca01ba072f6968f13aa2db97d1f0
Sara Pettersson <sara@doggie-zen.se>
2017-05-10 15:51:08 +0200

    SplitExpenseVM

--- Grupp5/Models/ViewModels/SplitExpenseVM.cs
+++ Grupp5/Models/ViewModels/SplitExpenseVM.cs
@@ -5,37 +5,12 @@ using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace Grupp5.Models.ViewModels
+namespace Grupp5.Models.Entities
 {
     public class SplitExpenseVM
     {
 		public SelectListItem[] CurrencyItem { get; set; }
 
-		[Required(ErrorMessage = "Obligatoriskt fält")]
-		[Display(Name = "Standardvaluta")]
-		public string SelectedCurrency { get; set; }
-
-		[Required(ErrorMessage = "Obligatoriskt fält")]
-		[Display(Name = "Event")]
-		public string FirstName { get; set; }
-
-		[Required(ErrorMessage = "Obligatoriskt fält")]
-		[Display(Name = "Efternamn")]
-		public string LastName { get; set; }
-
-		[Required(ErrorMessage = "Obligatoriskt fält")]
-		[Display(Name = "E-post")]
-		[DataType(DataType.EmailAddress)]
-		public string Email { get; set; }
-
-		[Required(ErrorMessage = "Obligatoriskt fält")]
-		[Display(Name = "Lösenord")]
-		[DataType(DataType.Password)]
-		[MinLength(5, ErrorMessage = "Minst 5 tecken krävs")]
-		public string Password { get; set; }
-
-		[Required(ErrorMessage = "Obligatoriskt fält")]
-		[Display(Name = "Lösenord")]
-		public string PasswordCheck { get; set; }
+		
 	}
 }
