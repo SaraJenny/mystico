aa756480624e611824ead5e86fa2a06f5852e21f
Sara Pettersson <sara@doggie-zen.se>
2017-05-10 14:58:24 +0200

    CSS, _Layout, plustecken, lagt till JS-fil

---
+++ Grupp5/wwwroot/js/script.js
@@ -0,0 +1,11 @@
+﻿$(document).ready(function () {
+	/*
+	MENY
+	*/
+	/* Togglar menyn */
+	$('#toggle-nav').click(function (e) {
+		e.preventDefault();
+		$(this).toggleClass('active');
+		$('#mainmenu ul').toggleClass('active');
+	});
+});
