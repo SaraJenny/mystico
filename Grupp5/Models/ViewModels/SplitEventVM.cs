daee468126c66fb5b1488889427daeee0cf5b059
Sara Pettersson <sara@doggie-zen.se>
2017-05-10 07:47:11 +0200

    Lagt till SplitEventVM, lagt till html i Event-vyn, ändrat lite i login- och register-vyerna

---
+++ Grupp5/Models/ViewModels/SplitEventVM.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Grupp5.Models.Entities
+{
+    public class SplitEventVM
+    {
+		[Required(ErrorMessage = "Obligatoriskt fält")]
+		[Display(Name = "Namn")]
+		public string Name { get; set; }
+
+//		[Required(ErrorMessage = "Obligatoriskt fält")]
+		[Display(Name = "Beskrivning")]
+		[DataType(DataType.MultilineText)]
+		public string Description { get; set; }
+
+		// TODO Gör detta till en select
+		[Required(ErrorMessage = "Obligatoriskt fält")]
+		[Display(Name = "Standardvaluta")]
+		public string Currency { get; set; }
+
+		// TODO Lägg till vänner
+	}
+}
