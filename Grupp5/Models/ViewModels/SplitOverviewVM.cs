30fdf402b25e0b5ada601edf548368973010b8ec
Sara Pettersson <sara@doggie-zen.se>
2017-05-10 16:50:54 +0200

    Lagt till SplitOverviewVM + uppdaterat _Layout + CSS

---
+++ Grupp5/Models/ViewModels/SplitOverviewVM.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Grupp5.Models.Entities
+{
+    public class SplitOverviewVM
+    {
+		public string EventName { get; set; }
+		public int MyStatus { get; set; }
+		public int Total { get; set; }
+		public int MyTotal { get; set; }
+
+	}
+}
