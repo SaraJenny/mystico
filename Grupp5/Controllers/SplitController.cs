f5b42155201f49ab6374d5b7a02945bea62605eb
Administrator <Administrator@ACADEMY-6303PF1>
2017-05-17 15:48:03 +0200

    Johanna: details isactive

--- Grupp5/Controllers/SplitController.cs
+++ Grupp5/Controllers/SplitController.cs
@@ -42,10 +42,13 @@ namespace Grupp5.Controllers
             var expenses = mysticoContext.GetExpensesByEvent(id);
             expenses = expenses.OrderByDescending(e => e.Date).ToList();
 
+            var myEvent = mysticoContext.GetEventById(id);
+
             var objections = mysticoContext.GetObjectionsInEvent(id);
             List<SplitDetailsVM> viewModel = Library.ConvertExpenseToSplitDetailsVM(expenses, objections);
             ViewBag.CurrentUserId = user.Id;
-            ViewBag.EventName = mysticoContext.GetEventById(id).EventName;
+            ViewBag.EventName = myEvent.EventName;
+            ViewBag.IsActive = myEvent.IsActive;
 
             return View(viewModel);
         }
